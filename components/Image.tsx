e42319b3445aa03f9d86bb5f7bf0daa03d85ac6d
Arjun G U <arjun.gu@exodusintel.com>
2023-12-02 14:44:51 +0530

    uwu

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,7 +1,7 @@
-import NextImage, { ImageProps } from 'next/image'
+import NextImage, { ImageProps } from 'next/image';
 
 const Image = ({ src, ...rest }: ImageProps) => (
   <NextImage src={`/4n0nym4u5-blog${src}`} {...rest} />
-)
+);
 
-export default Image
+export default Image;
